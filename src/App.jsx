c8bd7ead899ec45ccc66535116d81b517c12b4fb
Utazi Fredrick <utazifredrick530@gmail.com>
2026-09-11 13:50:42 +0100

    updated project name

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
-import { ProfileCard } from "./ProfileCard";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Counter App</h1>
+    </div>
+  );
 }
 
 export default App;
